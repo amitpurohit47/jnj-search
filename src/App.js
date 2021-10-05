c73dc2e6fc9e3c109826834c58bbf46fa730ccbe
Amit Purohit <amitpurohit4747@gmail.com>
2021-10-05 13:22:39 +0530

    added download api

--- src/App.js
+++ src/App.js
@@ -113,15 +113,31 @@ function App() {
     }
   };
 
-
+  const downloadReport = async () => {
+    axios({
+      url: "https://source.unsplash.com/random/500x500",
+      method: "GET",
+      responseType: "blob",
+    })
+      .then((response) => {
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", "file.csv");
+        document.body.appendChild(link);
+        link.click();
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div className="App">
       <img src={jjlogo} alt="logo" className="jj-logo" />
       <button
         className={`download-result ${
-          downloadAvailable ? "download-active" : null
+          downloadAvailable ? "download-active" : ""
         }`}
+        onClick={downloadReport}
       >
         <img src={download} alt="download" />
         Download
